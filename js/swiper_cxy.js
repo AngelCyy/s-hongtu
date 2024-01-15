841836d73361448ad8c0e58f1cbb2a5a632c4748
依依大神 <oncwnuCt6ITcD5eUbqLHXqmPIUp4@git.weixin.qq.com>
2024-01-15 09:08:34 +0800

    1

--- js/swiper_cxy.js
+++ js/swiper_cxy.js
@@ -49,3 +49,27 @@ var swiper1 = new Swiper('.swiper-container1', {
   effect: "slide",
 
 });
+var swiper2 = new Swiper('.swiper-container2', {
+  pagination: '.swiper-pagination',
+  //设置轮播的循环方式
+  loop: true,
+  //设置自动播放间隔时间
+  //设置自动播放间隔时间
+  autoplay: 3500,
+  // 轮播效果,默认为平滑轮播
+  effect: "slide",
+  paginationClickable: true,
+
+});
+var swiper3 = new Swiper('.swiper-container3', {
+  pagination: '.swiper-pagination',
+  //设置轮播的循环方式
+  loop: true,
+  //设置自动播放间隔时间
+  //设置自动播放间隔时间
+  // autoplay: 3500,
+  // 轮播效果,默认为平滑轮播
+  effect: "slide",
+  paginationClickable: true,
+
+});
